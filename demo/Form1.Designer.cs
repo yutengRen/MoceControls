e5143377cc4174030be9330cecc3a1d73dcfd9b7
yuteng ren <1203274720@qq.com>
2019-07-11 16:08:39 +0800

    全部上传

---
+++ demo/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace demo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(903, 258);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "第一个容器";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Location = new System.Drawing.Point(13, 295);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(903, 258);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "第二个容器";
+            // 
+            // textBox1
+            // 
+            this.textBox1.AllowDrop = true;
+            this.textBox1.Location = new System.Drawing.Point(502, 217);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.textBox1_MouseDown);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AllowDrop = true;
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(153, 199);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(78, 16);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.checkBox1_MouseDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(928, 608);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ demo/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace demo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // 默认为 false,即不接受用户拖动到其上的控件
+            this.groupBox1.AllowDrop = true;
+            this.groupBox2.AllowDrop = true;
+            // 拖动对象进入控件边界时触发
+            this.groupBox1.DragEnter += new DragEventHandler(groupBox_DragEnter);
+            this.groupBox2.DragEnter += new DragEventHandler(groupBox_DragEnter);
+            // 完成拖动时触发
+            this.groupBox1.DragDrop += new DragEventHandler(groupBox_DragDrop);
+            this.groupBox2.DragDrop += new DragEventHandler(groupBox_DragDrop);
+        }
+
+        /// <summary>
+        /// 该指标指示被拖动的对象是否进入了控件的边界
+        /// </summary>
+        bool bMouseDown;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CreateControls();
+        }
+
+        /// <summary>
+        /// 生成一定数量的控件,本例中使用 Button
+        /// 注册 Button 的鼠标点击事件
+        /// </summary>
+        private void CreateControls()
+        {
+            int x = 15;
+            int y = 15;
+            Button btn = null;
+            for (int i = 1; i <= 5; i++)
+            {
+                btn = new Button();
+                btn.Left = x;
+                btn.Top = y;
+                btn.Text = "Button " + i;
+                btn.Width = 100;
+                btn.Height = 50;
+                x += btn.Width + 15;
+                if (btn.Width > groupBox1.Width - x)
+                {
+                    x = 15;
+                    y += btn.Height + 15;
+                }
+                btn.AllowDrop = true; // 默认为 false,即不可拖动
+                btn.MouseDown += new MouseEventHandler(btn_MouseDown);
+                this.groupBox1.Controls.Add(btn);
+            }
+        }
+
+        /// <summary>
+        /// 拖动对象进入本控件的边界
+        /// </summary>
+        void groupBox_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Move;
+            bMouseDown = true;
+        }
+
+        /// <summary>
+        /// 拖放操作完成
+        /// </summary>
+        void groupBox_DragDrop(object sender, DragEventArgs e)
+        {
+            if (bMouseDown)
+            {
+                // 从事件参数 DragEventArgs 中获取被拖动的元素
+                string controlType = e.Data.GetFormats()[0];
+                System.Type type = Type.GetType(controlType + ", System.Windows.Forms, Version=1.0.3300.0, Culture=neutral, PublicKeyToken=b77a5c561934e089 ");
+                Control btn = (Control)e.Data.GetData(type);
+                GroupBox grp = (GroupBox)btn.Parent;
+                ((GroupBox)sender).Controls.Add(btn);
+                Point newPoint = PointToScreen(((GroupBox)sender).Location);
+                btn.Location = new Point(e.X - newPoint.X, e.Y - newPoint.Y);
+                bMouseDown = false;
+            }
+        }
+
+        /// <summary>
+        /// 按下鼠标后即开始执行拖放操作
+        /// 这里指定了拖放操作的最终效果为一个枚举值: Move
+        /// </summary>
+        void btn_MouseDown(object sender, MouseEventArgs e)
+        {
+            Control btn = (Control)sender;
+            btn.DoDragDrop(btn, DragDropEffects.Move);
+        }
+
+        private void textBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            Control con = (Control)sender;
+            con.DoDragDrop(con, DragDropEffects.Move);
+        }
+
+        private void checkBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            Control con = (Control)sender;
+            con.DoDragDrop(con, DragDropEffects.Move);
+        }
+    }
+}
